9ff8544151b8d78c3bf9cc991c5ddf0bbf2f6e6b
fadiabji <fadi.abji@htomail.com>
2022-10-19 12:08:39 +0200

    Query syantex and compare it with Linq statement, view will expect a list value then if they get a single value then get an error.

--- DbFirst/Controllers/MovieController.cs
+++ DbFirst/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DbFirst.Models;
+using System.Net.WebSockets;
 
 namespace DbFirst.Controllers
 {
@@ -11,41 +12,65 @@ namespace DbFirst.Controllers
         {
             
             //Select * from Movies
-            // first context
+            // first syntex
             var movies = db.Movies.ToList();
-            // second context
-            var QSMovies = (from m in db.Movies select m ).ToList(); 
+            // second syntex
+            var QSMovies = (from m in db.Movies 
+                            select m ).ToList(); 
+
 
             //Select top(3) from Movies
             //Order by price desc 
-            //First context
+            //First syntex
             var Top3ExpMovies = db.Movies.OrderByDescending(m => m.Price).Take(3).ToList();
-            //Second context
-            var QSTop3ExpMovies = (from m in db.Movies select m).OrderByDescending(m => m.Price).Take(3).ToList();
+            //Second syntex
+            var QSTop3ExpMovies = (from m in db.Movies 
+                                   select m).OrderByDescending(m => m.Price).Take(3).ToList();
 
 
             //top2cheapest
-            //First context
+            //First syntex
             var Top2CheapestMovies = db.Movies.OrderBy(m => m.Price).Take(2).ToList();
-            //Second context
-            var QSTop2CheapestMovies = (from m in db.Movies select m).OrderBy(m => m.Price).Take(2).ToList();
+            //Second syntex
+            var QSTop2CheapestMovies = (from m in db.Movies 
+                                        select m).OrderBy(m => m.Price).Take(2).ToList();
 
 
             //top2Oldest
-            //First context
+            //First syntex
             var Top2OldestsMovies = db.Movies.OrderBy(m => m.ReleaseYear).Take(2).ToList();
-            //Second Context
-            var QSTop2OldestsMovies = (from m in db.Movies select m).OrderBy(m => m.ReleaseYear).Take(2).ToList();
+            //Second syntex
+            var QSTop2OldestsMovies = (from m in db.Movies
+                                       select m).OrderBy(m => m.ReleaseYear).Take(2).ToList();
 
 
             //top2Newest
-            //First Context
+            //First syntex
             var Top2NewestsMovies = db.Movies.OrderByDescending(m => m.ReleaseYear).Take(2).ToList();
-            //Second Context
-            var QsTop2NewestsMovies = (from m in db.Movies select m).OrderByDescending(m => m.ReleaseYear).Take(2).ToList();
+            //Second syntex
+            var QsTop2NewestsMovies = (from m in db.Movies
+                                       select m).OrderByDescending(m => m.ReleaseYear).Take(2).ToList();
 
 
             return View(QsTop2NewestsMovies);
         }
+
+        public IActionResult WhereDemo()
+        {
+            // movies whose price is below 150;
+            var movies = db.Movies.Where(m => m.Price <= 150).ToList();
+            //Second syntax SQL syntax
+            var QsMovies = (from m in db.Movies 
+                            where m.Price < 150
+                            select m).ToList();
+
+            var movie = db.Movies.FirstOrDefault(m => m.Title =="Pulp Fiction"); // Here we will get a single value if we put it in view here 
+            // will get an error becuase the view expect a list of movies 
+
+            //return View(movie);
+
+            return View(movies);
+        }
+
     }
 }
